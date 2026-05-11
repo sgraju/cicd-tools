2e29b6518d9a0e6c01528552d0431320bdc94db5
sgraju <sgraju.tibco@gmail.com>
2026-05-11 19:49:02 +0400

    cicd-tools

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -46,7 +46,7 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-eksctl and kubectl
+# eksctl and kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl  /usr/local/bin && export PATH=$HOME/bin:$PATH
