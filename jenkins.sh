c6379cd658a10ae9bf44ec8ce0cf0ed7460837e7
sgraju <sgraju.tibco@gmail.com>
2026-05-11 21:15:32 +0400

    cicd-tools

--- jenkins.sh
+++ jenkins.sh
@@ -12,7 +12,7 @@ xfs_growfs /var
 xfs_growfs /home
 
 
-curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+curl -fsSL https://pkg.jenkins.io/rpm-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-21-openjdk -y
 yum install jenkins -y
