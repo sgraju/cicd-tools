740711a2cea5e90526ee0cdb62ac4858cdee174a
sgraju <sgraju.tibco@gmail.com>
2026-05-11 21:33:41 +0400

    cicd-tools

--- jenkins.sh
+++ jenkins.sh
@@ -12,12 +12,12 @@ xfs_growfs /var
 xfs_growfs /home
 
 
-curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.repo \
+sudo curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.repo \
 -o /etc/yum.repos.d/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-21-openjdk -y
-yum repolist | grep jenkins
-yum install jenkins -y
-systemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install fontconfig java-21-openjdk -y
+sudo yum repolist | grep jenkins
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
